b3d593b2a10e2e2cfb8674d1d256c5349be41976
ArcherL <liuzihao2008@gmail.com>
2025-12-17 02:26:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,9 +38,9 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 # 安装uugamebooster
 rm -rf feeds/luci/applications/luci-app-uugamebooster
-git clone https://github.com/datouha/uugamebooster/tree/main/luci-app-uugamebooster package/lean/luci-app-uugamebooster
+git clone https://github.com/datouha/uugamebooster/luci-app-uugamebooster package/lean/luci-app-uugamebooster
 rm -rf feeds/packages/net/uugamebooster
-git clone https://github.com/datouha/uugamebooster/tree/main/uugamebooster feeds/packages/net/uugamebooster
+git clone https://github.com/datouha/uugamebooster/uugamebooster feeds/packages/net/uugamebooster
 
 
 
